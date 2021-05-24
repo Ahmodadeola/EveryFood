970ad0917493fe3e909ba494333b5e2f863426fc
ahmodadeola <ahmodadeola@gmail.com>
2021-05-24 23:12:57 +0100

    animated input label

--- src/components/auth/Input.js
+++ src/components/auth/Input.js
@@ -1,4 +1,5 @@
 import React, { useCallback, useReducer } from "react";
+import useInput from "../../hooks/useInput";
 
 const Input = (props) => {
   const {
@@ -7,37 +8,34 @@ const Input = (props) => {
     options,
   } = props;
 
-  //   const reducer = (focused, v) => {
-  //     reyrun;
-  //   };
+  const { show, error, changeHandler } = useInput({
+    config: props.config,
+  });
 
   const formatLabel = useCallback((text) => {
     return text.replace(/[-_]/g, " ");
   }, []);
 
-  const [focused, focusHandler] = useReducer(
-    (focused, e) => !!e.target.value,
-    false
-  );
-
   let input = <input />;
+  let labelClasses =
+    `text-lg text-green-400 text-medium block text-left transition
+  transform ease-in duration-400`.split(" ");
+  labelClasses = show
+    ? [...labelClasses, "translate-y-0", "h-auto"]
+    : [...labelClasses, "translate-y-9", "invisible", "h-0"];
+
   switch (inputType) {
     case "input":
       let fmtName = formatLabel(name);
-      console.log(fmtName);
+
       return (input = (
         <div className="">
-          {
-            <label
-              className="text-lg text-green-400 text-medium block text-left"
-              htmlFor={name}
-            >
-              {focused && fmtName[0].toUpperCase() + fmtName.slice(1)}
-            </label>
-          }
+          <label className={labelClasses.join(" ")} htmlFor={name}>
+            {fmtName[0].toUpperCase() + fmtName.slice(1)}
+          </label>
           <input
             {...props}
-            onFocus={focusHandler}
+            onChange={changeHandler}
             id={name}
             type={type}
             placeholder={fmtName[0].toUpperCase() + fmtName.slice(1)}
--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,9 +1,40 @@
-const useInput = ({ config }) => {
-    const valid = false;
-    const focusHandler = () => ;
-    return {
-        valid,
+import { useEffect, useState } from "react";
+
+const useInput = ({ config: { rules } }) => {
+  const [show, toggle] = useState(false);
+  const [error, setError] = useState("");
+  const [value, setValue] = useState("");
+  const valid = false;
+
+  useEffect(() => {
+    toggle(!!value);
+    for (let rule of rules) {
+      //   rule = (v) =>
+      //     v.length > 6 || "Password must not be less than 6 characters";
+      //   console.log(rule.toString());
+      let rs = rule("salau");
+      console.log(rs);
+
+      if (typeof result === String) {
+        setError("");
+        // console.log(`[useInput hook]Error is => ${result}`);
+        break;
+      }
     }
+  }, [value]);
+
+  const focusHandler = () => {
+    return show;
+  };
+  const changeHandler = (e) => {
+    setValue(e.target.value);
+  };
+  return {
+    value,
+    error,
+    show,
+    changeHandler,
+  };
 };
 
-export { useInput };
+export default useInput;
