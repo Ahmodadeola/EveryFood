ed898636b0be876bd5050aa311008adbaeb9f50e
ahmodadeola <ahmodadeola@gmail.com>
2021-05-27 09:20:41 +0100

    created authSlice and updated store

--- src/store/reducers/authSlice.js
+++ src/store/reducers/authSlice.js
@@ -17,18 +17,49 @@ const AuthSlice = createSlice({
   name: "Auth",
   initialState,
   reducers: {
-    login(state, { data }) {
-      return;
+    startLoad(state) {
+      state.loading = true;
+      state.error = null;
     },
-    signup(state, { data }) {
-      return;
+
+    loginSuccess(state, { data }) {
+      state.loading = false;
+      state.userInfo = data;
     },
-    resetPassword(state, { data }) {
-      return;
+
+    loginFailed(state, { error }) {
+      state.laoding = false;
+      state.error = error;
+    },
+    signupSuccess(state, { data }) {
+      state.loading = false;
+    },
+
+    signupFailed(state, { error }) {
+      state.loading = false;
+      state.error = error;
+    },
+    resetPasswordSuccess(state, { data }) {
+      state.data = data;
+    },
+
+    resetPasswordFailed(state, { error }) {
+      state.loading = false;
+      state.error = error;
     },
   },
 });
 
 const { reducer, actions } = AuthSlice;
+
+export const {
+  startLoad,
+  loginSuccess,
+  loginFailed,
+  signupFailed,
+  signupSuccess,
+  resetPasswordSuccess,
+  resetPasswordFailed,
+} = actions;
 
 export default reducer;
