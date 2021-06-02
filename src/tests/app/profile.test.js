d60480786158071980421751b6768e4f5001b15e
ahmodadeola <ahmodadeola@gmail.com>
2021-06-02 11:58:13 +0100

    fixed failing test

--- src/tests/app/profile.test.js
+++ src/tests/app/profile.test.js
@@ -2,11 +2,23 @@ import { render } from "@testing-library/react";
 import Dishes from "../../pages/app/dishes";
 
 describe("Dishes page test", () => {
-  test("Check", () => {
+  const links = ["Dishes", "Tray", "Profile", "Vendor"];
+  test("NavBar links check", () => {
     const { getByText } = render(<Dishes />);
     getByText("EveryFood");
-    ["Dishes", "Tray", "Profile", "Vendor"].forEach((item) => {
-      getByText(item);
+
+    // links on desktop Navbar
+    [...document.querySelectorAll("nav.hidden a")].forEach((el, idx) => {
+      expect(el.textContent).toBe(links[idx]);
+    });
+
+    const dishLink = [...document.querySelectorAll("nav.hidden a>li")][0];
+    expect(dishLink).toHaveClass(
+      "text-md cursor-pointer font-medium text-gray-500 border-solid border-white border-r-4 hover:text-indigo-500 hover:bg-indigo-100 p-3 text-green-500 border-green-500 bg-indigo-100"
+    );
+    // links on mobile Navbar
+    [...document.querySelectorAll("div>ul a")].forEach((el, idx) => {
+      expect(el.textContent).toBe(links[idx]);
     });
   });
 });
