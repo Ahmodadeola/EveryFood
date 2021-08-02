77dd3db0966b152298b0caf0e7afe5d06058147a
ahmodadeola <ahmodadeola@gmail.com>
2021-08-02 01:54:35 +0100

    added tray items notification

--- src/components/home/NavItems.js
+++ src/components/home/NavItems.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function NavItems({ items, className, close = () => null }) {
+  const { tray } = useSelector((state) => state.dish);
   let allClasses = className;
   return (
     <>
@@ -9,9 +11,15 @@ function NavItems({ items, className, close = () => null }) {
         allClasses = item.addStyle
           ? allClasses.concat(" " + item.addStyle)
           : className;
+        allClasses += " relative";
         return (
           <Link to={item.link} key={idx} onClick={close}>
             <li className={allClasses}>
+              {item.title === "Tray" && tray.length > 0 && (
+                <span className="absolute flex items-center justify-center text-yellow-300 rounded-full h-5 w-5 top-1 text-xs font-bold bg-green-700">
+                  {tray.length}
+                </span>
+              )}
               {item.icon && (
                 <item.icon
                   className={`inline-block ml-3 mr-5 ${
